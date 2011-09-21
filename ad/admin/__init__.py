cfc2cd9df75fad6c39a96cf6d2e85f3d80910f2d
Lincoln de Sousa <lincoln@comum.org>
2011-09-21 15:39:13 -0300

    Adicionando algumas views http para gerenciar Audiências
    A única view que não dá problema atualmente é a "listing",
    as outras estão quebradas por não terem templates ainda.

---
+++ ad/admin/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2011  Governo do Estado do Rio Grande do Sul
+#
+#   Author: Lincoln de Sousa <lincoln@gg.rs.gov.br>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Module that uses the Template and Model APIs to build the Admin web
+interface.
+"""
+
+from flask import Blueprint, render_template
+
+admin = Blueprint('admin', __name__, template_folder='templates')
+
+@admin.route('/')
+def index():
+    """Main view, lists all registered audiencces"""
+    return render_template('admin/listing.html')
+
+@admin.route('/audience/new')
+def new():
+    """Shows the form that creates new audiences and save collected data
+    in the database.
+    """
+    return render_template('admin/new.html')
+
+@admin.route('/audience/<int:aid>')
+def audience(aid):
+    """Returns a form to edit an audience and saves new params in the
+    database.
+    """
+    return render_template('admin/edit.html')
