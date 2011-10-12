403d95e5f2dc3f245b3eac3bab4e423216dfd6ff
Lincoln de Sousa <lincoln@comum.org>
2011-10-12 15:55:51 -0300

    Primeiro draft da API de autenticação em javascript
    Esta API provê as seguintes funcionalidades públicas atualmente:
    
     • isAuthenticated()
     • authenticatedUser()
     • showLoginForm()

--- ad/auth/static/js/auth.js
+++ ad/auth/static/js/auth.js
@@ -16,10 +16,51 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* Namespace for authentication stuff */
+var auth = (function() {
 
-$(function() {
+    /** Class that unifies the main things about authentication */
+    function Auth() { }
+
+    Auth.prototype = {
+        /** Place to store cached authentication data. Actually, the
+         * whole authenticated user information. */
+        user: null
+
+        /** Callback that will be fired after a successful
+         *  authentication */
+        , success: function () {}
+
+        /** Returns True if there is someone authenticated */
+        , isAuthenticated: function () {
+            return this.user !== null;
+        }
+
+        /** Returns the current authenticated user */
+        , authenticatedUser: function () {
+            return this.user;
+        }
+
+        /** Shows the login form and register callbacks to be called when
+         *  it returns successfuly or not */
+        , showLoginForm: function (params) {
+            $('a[rel=#loginoverlay]').click();
+            if (typeof params.success === 'function') {
+                this.success = params.success;
+            }
+        }
+
+        /** Method called after a successful authentication. It saves
+         *  the authentication information and makes sure that the user
+         *  interface will be updated showing authentication info. */
+        , userAuthenticated: function (user) {
+            this.user = user;
+            this.success();
+        }
+    };
+
     /* Overlay for all <a> tags with a `rel' attribute */
-    $('a[rel]').overlay({
+    $('a[rel=#loginoverlay]').overlay({
         top: 250,
         mask: {
             color: '#fff',
@@ -51,10 +92,13 @@ $(function() {
                         overlay.find('div.error').html(data.msg).fadeIn('fast');
                     } else {
                         closeMethod();
+                        auth.userAuthenticated(data.msg.user);
                     }
                 });
                 return false;
             });
         }
     });
-});
+
+    return new Auth();
+})();
