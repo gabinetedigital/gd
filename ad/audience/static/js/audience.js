8876cdedabe3d62e965678cff20851df54f77a8a
Lincoln de Sousa <lincoln@comum.org>
2011-10-12 16:00:02 -0300

    Automatização JS do formulário de nova notícia no buzz via site
    Agora, quando o usuário postar alguma notícia pelo textarea em baixo do
    buzz sem estar autenticado, verá o form de autenticação e o conteúdo será
    enviado depois disso para o server (/buzz/post).
    
    A inda estou trabalhando no feedback para o usuário sobre uma possível
    mensagem de erro e na mensagem de sucesso e agradecimento.

--- ad/audience/static/js/audience.js
+++ ad/audience/static/js/audience.js
@@ -35,6 +35,68 @@ $(function() {
         });
     });
 
+
+    /** Posts a new notice on the message buzz */
+    function postNotice(message) {
+        var params = { aid: $('#aid').val(), message: message };
+        $.post(url_for('buzz.post'), params, function (data) {
+            var parsedData = $.parseJSON(data);
+            if (parsedData.status !== 'ok') {
+                // For any reason, the user got loged out, so, as it's
+                // an async call, we have to request login credentials
+                // again. It's not actually usual to happen but better
+                // being safe than sorry.
+                if (parsedData.code === 'NobodyHome') {
+                    auth.shpowLoginForm({
+                        success: function (userData) {
+                            postNotice(message);
+                        }
+                    });
+                    return;
+                }
+
+                // Feedback the user. something wrong happened.
+            } else {
+                // Everything' fine, let's just clear the message box
+                // and thank the user
+                var $textbox = $("#internal_chat textarea");
+                $textbox.val('');
+            }
+        });
+    }
+
+
+    $('#internal_chat').submit(function () {
+        var $form = $(this);
+        var $field = $('textarea', $form);
+        var aid = $('#aid').val();
+        var data = $.trim($field.val());
+
+        // None received in the form. We cannot process this
+        // request. Just feedback the user.
+        if (data === '') {
+            $field.addClass('error');
+            return false;
+        }
+
+        // Just making sure that the user will not be confused by any
+        // old error report.
+        $field.removeClass('error');
+
+        // Not authenticated users must access the login form
+        if (!auth.isAuthenticated()) {
+            auth.showLoginForm({
+                success: function (userData) {
+                    postNotice(data);
+                }
+            });
+        } else {
+            // Time to send the request to the server
+            postNotice(data);
+        }
+        return false;
+    });
+
     // Starts a new instance of the buzz stream
 
     function updateBuzz(msg, show) {
