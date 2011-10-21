dbfa76753d5280c74372b33c2d7130d055cde90b
Lincoln de Sousa <lincoln@comum.org>
2011-10-21 14:28:08 -0200

    Primeiros passos de um cliente XMLRPC para o Wordpress
    Este cliente xmlrpc vai ser utilizado para montar a capa e o blog.
    Atualmente, ele só interage com a API extendida que o plugin `exapi'
    disponibiliza. Mas, assim que for necessário, será transformado
    numa interface genérica para as chamadas das APIs padrão do
    wordpress.

---
+++ ad/content/wp.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2011  Governo do Estado do Rio Grande do Sul
+#
+#   Author: Lincoln de Sousa <lincoln@gg.rs.gov.br>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Module to carry about all interaction between wordpress XMLRPC
+interface and our system.
+"""
+
+from xmlrpclib import Server
+from datetime import datetime
+
+
+class Wordpress(object):
+    """Wordpress XMLRPC client"""
+    def __init__(self, address, blogid, user, password):
+        self.blogid = blogid
+        self.address = address
+        self.user = user
+        self.password = password
+        self.server = Server(self.address)
+
+    def wrap(self, method):
+        """Wrapper that decorates XMLRPC methods that needs the user,
+        password and blog id to be passed before anything"""
+        def wrapper(*args, **kwargs):
+            # Yes, I'm aware that `kwargs' is not being expanded when
+            # called in the next line. The params are being sent packed
+            # to the XMLRPC server and there they'll be expanded.  Maybe
+            # it will change in the future, if I find any function that
+            # doesn't fit this strategy
+            return method(self.blogid, self.user, self.password,
+                          kwargs, *args)
+        return wrapper
+
+    def __getattribute__(self, attr):
+        try:
+            return super(Wordpress, self).__getattribute__(attr)
+        except AttributeError:
+            return self.wrap(getattr(self.server.exapi, attr))
