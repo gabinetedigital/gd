b79df6afcd46c1c078d1d16b3625c402c92f71b9
Lincoln de Sousa <lincoln@comum.org>
2011-10-26 21:47:17 -0200

    Pequenas melhorias cosméticas no modulo `content.app'

--- ad/content/app.py
+++ ad/content/app.py
@@ -19,24 +19,17 @@
 implemented blueprints in various modules to this app.
 """
 
-if __name__ == '__main__':
-    import sys
-    # Ugly but will help us to run the application in debug mode when we
-    # need. This way, the socketio and all the intercommunication stuff
-    # will not be started. The ad-d script will be responsible for that
-    # step when it is needed.
-    sys.path.append('..')
-
-
 from flask import Flask, request
+
+from ad.auth import authenticated_user, NobodyHome
+from ad import conf
+
 from ad.admin import admin
 from ad.audience import audience
 from ad.auth.webapp import auth
 from ad.buzz.webapp import buzz
 from ad.buzz.facebookapp import fbapp
 
-from ad.auth import authenticated_user, NobodyHome
-from ad import conf
 
 app = Flask(__name__)
 app.register_blueprint(admin, url_prefix='/admin')
@@ -44,6 +37,10 @@ app.register_blueprint(buzz, url_prefix='/buzz')
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(audience, url_prefix='/audience')
 app.register_blueprint(fbapp, url_prefix='/fbapp')
+
+
+# Registering a secret key to be able to work with sessions
+app.secret_key = conf.SECRET_KEY
 
 
 @app.context_processor
@@ -66,12 +63,3 @@ def extend_context():
 
     # Job done!
     return context
-
-
-# Registering a secret key to be able to work with sessions
-import os
-app.secret_key = conf.SECRET_KEY
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
