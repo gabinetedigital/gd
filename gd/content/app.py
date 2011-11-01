fd3bdf6dfd566466ed05160594adc35ab28b06d2
Lincoln de Sousa <lincoln@comum.org>
2011-11-01 12:00:47 -0200

    Carregando o módulo de configuração pra app do flask

--- gd/content/app.py
+++ gd/content/app.py
@@ -28,6 +28,7 @@ from gd.admin import admin
 from gd.audience import audience
 from gd.auth.webapp import auth
 from gd.buzz.webapp import buzz
+from gd.auth.fbauth import fbauth
 from gd.buzz.facebookapp import fbapp
 from gd.govpergunta import govpergunta
 
@@ -36,13 +37,16 @@ app = Flask(__name__)
 app.register_blueprint(admin, url_prefix='/admin')
 app.register_blueprint(buzz, url_prefix='/buzz')
 app.register_blueprint(auth, url_prefix='/auth')
+app.register_blueprint(fbauth, url_prefix='/auth/fb')
 app.register_blueprint(audience, url_prefix='/audience')
 app.register_blueprint(fbapp, url_prefix='/fbapp')
 app.register_blueprint(govpergunta, url_prefix='/govpergunta')
 
-
 # Registering a secret key to be able to work with sessions
 app.secret_key = conf.SECRET_KEY
+
+# Loading the config variables from our `gd.conf' module
+app.config.from_object(conf)
 
 
 @app.context_processor
