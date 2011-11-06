5a61aaddb916194554e03a4dc961a77c7629e567
Lincoln de Sousa <lincoln@comum.org>
2011-11-06 13:07:48 -0200

    (govp) Transformando as tabs em um wizard e acertando a navegação
    Além disso, outras correções de layout foram feitas, como o aumento do
    tamanho do container, para encaixar as imagens de temas.

--- gd/govpergunta/static/js/govpergunta.js
+++ gd/govpergunta/static/js/govpergunta.js
@@ -1,3 +1,36 @@
+var navapi = null;
+
 $(function () {
-    var wizard = $("ol.tabs").tabs("div.panes > div");
+    var wizard = $('.wizard').data('isLoaded', false);
+
+    $('#maintext, .tabs').click(function() {
+        wizard.expose({ color:'#ddd', lazy:true });
+        $('html,body').animate({ scrollTop: 320 }, 150);
+    });
+
+    $("ol.tabs", wizard).tabs(
+        "div.panes > div", {
+            effect: 'fade',
+            lazy: true,
+            onBeforeClick: function () {
+                if (wizard.data('isLoaded')) {
+                    $('html,body').animate({ scrollTop: 320 }, 150);
+                }
+            }
+        });
+    wizard.data('isLoaded', true);
+
+    navapi = $("ol.tabs", wizard).data("tabs");
+
+    // "next tab" button
+    $(".next", wizard).click(function () {
+        $('html,body').animate({ scrollTop: 320 }, 100);
+	navapi.next();
+    });
+
+    // "previous tab" button
+    $(".prev", wizard).click(function () {
+        $('html,body').animate({ scrollTop: 320 }, 100);
+	navapi.prev();
+    });
 });
