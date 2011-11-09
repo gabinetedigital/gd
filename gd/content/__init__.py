389023635eb836d7470c67c892a691cced2b6de5
Lincoln de Sousa <lincoln@comum.org>
2011-11-09 20:24:44 -0200

    Removendo referências à aplicação "audience"
    Com isso, as dependências "zmq", "gevent", "socketio" foram removidas
    também. Elas tiveram um péssimo desempenho no servidor e ameaçam
    a estabilidade da aplicação.

--- gd/content/__init__.py
+++ gd/content/__init__.py
@@ -28,26 +28,18 @@ from flask import Flask, request, render_template, session, \
 from gd import conf
 from gd.auth import is_authenticated, authenticated_user, NobodyHome
 from gd.content.wp import wordpress
-from gd.model import session as dbsession
 from gd.utils import dumps
-
-from gd.admin import admin
-from gd.audience import audience
-from gd.auth.fbauth import fbauth
-from gd.auth.webapp import auth
-from gd.buzz.webapp import buzz
-from gd.buzz.facebookapp import fbapp
-from gd.govpergunta import govpergunta
+from gd.model import session as dbsession
 from gd.model import get_mayor_last_tweet, User
+
+from gd.auth.webapp import auth
+from gd.auth.fbauth import fbauth
+from gd.govpergunta import govpergunta
 from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
-app.register_blueprint(admin, url_prefix='/admin')
-app.register_blueprint(buzz, url_prefix='/buzz')
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(fbauth, url_prefix='/auth/fb')
-app.register_blueprint(audience, url_prefix='/audience')
-app.register_blueprint(fbapp, url_prefix='/fbapp')
 app.register_blueprint(govpergunta, url_prefix='/govpergunta')
 
 # Registering a secret key to be able to work with sessions
