f1c608cec6b2146f8e0edc517c69011c33078903
Lincoln de Sousa <lincoln@comum.org>
2011-11-09 04:21:22 -0200

    (layout) Adicionando links do menu "Participe" (Closes: #222)
    Recriei o html e mexi nos seletores de identificação (classes e ids) pra
    evitar colisões bizarras com nomes muito comuns como "govresponde" ou
    "agenda".

--- gd/content/static/js/site.js
+++ gd/content/static/js/site.js
@@ -1,10 +1,17 @@
 $(function () {
 
-  $("#govresponde").data('text','Quer saber o que o governador pensa? Pergunte para ele. A questão mais votada do mês é respondida em vídeo.');
-  $("#govpergunta").data('text', 'O Governador pergunta e a sociedade responde com novas idéias.');
-  $("#agenda").data('text', 'Quer fazer parte da agenda do governador na sua cidade? Indique uma pauta na agenda colaborativa.');
-  $("#govescuta").data('text','Audiências públicas digitais transmitidas via internet onde você pode enviar sua contribuição.');
-
+  $("#participe .govresponde")
+        .data('text','Quer saber o que o governador pensa? Pergunte para ele. ' +
+              'A questão mais votada do mês é respondida em vídeo.');
+  $("#participe .govpergunta")
+        .data('text', 'O Governador pergunta e a sociedade responde com novas ' +
+              'idéias.');
+  $("#participe .agenda")
+        .data('text', 'Quer fazer parte da agenda do governador na sua cidade?' +
+              ' Indique uma pauta na agenda colaborativa.');
+  $("#participe .govescuta")
+        .data('text','Audiências públicas digitais transmitidas via internet  ' +
+              'onde você pode enviar sua contribuição.');
   $("#participe").mouseenter(function() {
     if (home_menu_delay) clearTimeout(home_menu_delay);
   });
@@ -36,21 +43,21 @@ $(function () {
 
   $("#a-participe").hover(show_home_menu, prepare_to_hide_home_menu);
 
-  $("#govresponde, #govpergunta, #govescuta, #agenda").hover(function() {
-    var self = $(this);
-    $("#desc").text(self.data("text"));
-    var bg = self.css("background-image");
-    self.css("background-image",bg.replace("base","selected"));
-  }, function() {
-    $("#desc").text('');
-    var self = $(this);
-    var bg = self.css("background-image");
-    self.css("background-image",bg.replace("selected","base"));
-  });
+  $("#participe .govresponde, " +
+    "#participe .govpergunta, " +
+    "#participe .govescuta,   " +
+    "#participe .agenda")
+        .hover(function() {
+            $(this).addClass('selected');
+            $("#participe .desc").text($(this).data("text"));
+        }, function() {
+            $("#participe .desc").text('');
+            $(this).removeClass('selected');
+        });
 });
 
 function goto(url) {
   return function() {
     document.location.href = url;
-  }
+  };
 }
