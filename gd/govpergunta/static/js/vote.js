3a24f942a3985a7401d56c166d6f281885bf0224
Thiago Silva <thiago@metareload.com>
2011-11-24 16:39:17 -0200

    Implementando skip

--- gd/govpergunta/static/js/vote.js
+++ gd/govpergunta/static/js/vote.js
@@ -10,4 +10,10 @@ $(function() {
     $("#vote-direction").attr("value", "right");
     $("#vote-form").submit();
   });
+
+  $("#vote-skip").click(function(ev) {
+    ev.preventDefault();
+    $("#vote-direction").attr("value", "skip");
+    $("#vote-form").submit();
+  });
 });
