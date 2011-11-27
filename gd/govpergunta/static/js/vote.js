1cf1e9e3df62765f038f16debf0e03c80ac4f3ed
Lincoln de Sousa <lincoln@comum.org>
2011-11-27 19:29:40 -0200

    Adição do como funciona na página de votação

--- gd/govpergunta/static/js/vote.js
+++ gd/govpergunta/static/js/vote.js
@@ -16,4 +16,31 @@ $(function() {
     $("#vote-direction").attr("value", "skip");
     $("#vote-form").submit();
   });
+
+
+    $('.overlayt').attr('rel', function () {
+        return $(this).attr('href');
+    }).overlay({
+        oneInstance: false,
+        speed: 'fast',
+        mask: {
+            color: '#111',
+            opacity: 0.7
+        },
+        onBeforeLoad: function() {
+            var overlay = this.getOverlay();
+            var wrap = overlay.find(".contentWrap");
+            var closeMethod = this.close;
+            var url =
+                    url_for('pages.govpergunta.como-funciona-a-votacao') +
+                    '.json';
+            $.getJSON(url, function (data) {
+                /* We're done! */
+                overlay.removeClass('loading');
+
+                /* Loading the content */
+                wrap.html(data.content);
+            });
+        }
+    });
 });
