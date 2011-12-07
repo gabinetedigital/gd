d75d9adc812af6cd79e0f5cb7cbcd62e57f44213
Lincoln de Sousa <lincoln@comum.org>
2011-12-07 10:25:43 -0200

    Removendo os botões sociais da capa e acertando detalhes de html e js
    Movi os javascripts sociais pro arquivo site.js e movi o elemento fbroot do
    head pro body. Além de ter dado uma indentada nalguns htmls.

--- gd/content/static/js/site.js
+++ gd/content/static/js/site.js
@@ -90,4 +90,14 @@ $(function () {
             }
         });
     }
+
+    /* -- sociable stuff -- */
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
 });
