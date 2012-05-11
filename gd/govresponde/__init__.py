b2889205f4f3a1e615d236fd33050160eb48f266
Leo Andrade <leo-andrade@procergs.rs.gov.br>
2012-05-11 17:42:39 -0300

    novo layout govresponde

--- gd/govresponde/__init__.py
+++ gd/govresponde/__init__.py
@@ -70,7 +70,7 @@ def _get_context(custom=None):
 def _format_contrib(contrib):
     contrib['created_at'] = dateparser.parse(contrib['created_at'])
     contrib['answered_at'] = dateparser.parse(contrib['answered_at'])
-    #contrib['theme'] = wordpress.govr.getTheme(contrib['theme_id'])
+    contrib['theme'] = wordpress.govr.getTheme(contrib['theme_id'])
     contrib['video'] = wordpress.wpgd.getVideo(contrib['data'])
     contrib['video_sources'] = wordpress.wpgd.getVideoSources(contrib['data'])
     return contrib
