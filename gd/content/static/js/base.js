de3f7a56ca007547769df7eeb04ed1eee8b79e4c
Guilherme Guerra <guerrinha@comum.org>
2012-08-22 18:05:19 -0300

    (home) Adicionando elementos do cabeçalho do site

---
+++ gd/content/static/js/base.js
@@ -0,0 +1,37 @@
+$(function(){
+    var sticky_navigation_offset_top = $('.subnav').offset().top;
+    var sticky_navigation = function(){
+        var scroll_top = $(window).scrollTop();
+        if (scroll_top > sticky_navigation_offset_top) {
+            $('.subnav').addClass('subnav-fixed');
+            $('.tools').show();
+        } else {
+            $('.subnav').removeClass('subnav-fixed');
+            $('.tools').hide();
+        }  
+    };
+    sticky_navigation();
+    $(window).scroll(function() {
+        sticky_navigation();
+    });
+
+    $('input').focusin(function(){
+        $( "input" ).switchClass( "input-small", "input-big", 300 );
+        return false;
+    });
+
+    $('input').focusout(function(){
+        $( "input" ).switchClass( "input-big", "input-small", 300 );
+        return false;
+    });
+
+    option = {
+        title: '@andrenano',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lobortis ultricies nunc ac euismod. Class aptent taciti sociosqu ad volutpat.',
+        trigger: 'hover',
+        placement: 'right',
+    };
+    $('.tweets li').popover(option);
+
+    $('.subnav').scrollspy()
+});
