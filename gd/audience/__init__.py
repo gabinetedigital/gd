c150a21b1c773b80580cdbc0c1564804b8cd99ff
Leo Andrade <leo-andrade@procergs.rs.gov.br>
2012-08-31 17:43:44 -0300

    (govEscuta) Retirado uma linha de log

--- gd/audience/__init__.py
+++ gd/audience/__init__.py
@@ -15,7 +15,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from twisted.web.static import Redirect
 
 """Module that uses the Template and Model APIs to build the Audience web
 interface.
