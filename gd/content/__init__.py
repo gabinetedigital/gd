f5f20a2c4d32db6415f4da0dcc6b61731209ea4e
Sergio Berlotto <sergio-berlotto@gg.rs.gov.br>
2013-01-18 15:27:10 -0200

    Adicionadas rotas para páginas de erro customizadas

--- gd/content/__init__.py
+++ gd/content/__init__.py
@@ -78,6 +78,24 @@ app.jinja_env.install_gettext_callables(
 
 cache = Cache(app)
 
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html',
+        sidebar=wordpress.getSidebar,
+    ), 403
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html',
+        sidebar=wordpress.getSidebar,
+    ), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html',
+        sidebar=wordpress.getSidebar,
+    ), 500
+
 def _format_postsearch(posts):
     """" Retorna os campos para ser montado na tela:
         title, url, posttype, data, excerpt, textobotao, thumbs 
