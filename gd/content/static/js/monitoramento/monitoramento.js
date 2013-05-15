52cac678c29b469940a491565fa078aa2d9dd5c2
Guilherme Guerra <guerrinha@comum.org>
2013-05-15 10:54:55 -0300

    (monitoramento) Removendo sticky bar

--- gd/content/static/js/monitoramento/monitoramento.js
+++ gd/content/static/js/monitoramento/monitoramento.js
@@ -40,21 +40,6 @@ $(document).ready(function () {
         $('.voltar').fadeOut();
     });
 
-    var sticky_participe_offset_top = $('.follows').offset().top;
-    var sticky_participe = function(){
-        var scroll_top_part = $(window).scrollTop();
-        if (scroll_top_part > sticky_participe_offset_top) {
-            $('.follows').addClass('follows-fixed');
-        } else {
-            $('.follows').removeClass('follows-fixed');
-        }
-    };
-
-    sticky_participe();
-    $(window).scroll(function() {
-        sticky_participe();
-    });
-
     $(".seguirobra").fancybox();
     $(".botoesparticipar a").fancybox({
         afterLoad  : function(current, previous){
@@ -99,6 +84,5 @@ $(document).ready(function () {
     $('#part-imagem').ajaxForm({
         success:ret
     });
-
 
 });
