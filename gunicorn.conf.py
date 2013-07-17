c51de2a0754c768d00b2b92e25959d090f4a8f92
Sergio Berlotto <sergio-berlotto@gg.rs.gov.br>
2013-07-17 09:51:08 -0300

    Alterado worker class

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import os
 bind = '127.0.0.1:8000'
 workers = 8
 backlog = 2048
-worker_class = "tornado"
+worker_class = "gevent"
 debug = True
 proc_name = 'gunicorn.gd'
 pidfile = '/tmp/gunicorn.pid'
