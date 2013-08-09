3652f56d601fc6db6ebbcdee0b681ff9fc2d390a
Guilherme Guerra <guerrinha@comum.org>
2013-08-09 17:46:40 -0300

    (seminario) pagina de convidados

--- gd/seminario/static/js/seminario.js
+++ gd/seminario/static/js/seminario.js
@@ -1,3 +1,16 @@
 $(window).load(function() {
+    $('.section').css({'height':(($(window).height()))+'px'});
+
     $("#menu li a").tooltip();
+
+    $('#menu a[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+        var target = this.hash,
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
