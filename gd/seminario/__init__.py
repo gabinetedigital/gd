611b69b804ce4a6cd704b7c13fe38cc6936c7571
Sergio Berlotto <sergio-berlotto@gg.rs.gov.br>
2013-08-12 16:13:17 -0300

    Adicionada seção das fotos do flickr

--- gd/seminario/__init__.py
+++ gd/seminario/__init__.py
@@ -44,13 +44,48 @@ seminario = Blueprint(
     static_folder='static')
 
 
+def get_flickr_photos():
+    import flickrapi
+
+    api_key = conf.FLICKR_APP_KEY
+    api_secret = conf.FLICKR_APP_SECRET
+
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, cache=False)
+    
+    photos = flickr.photos_search(tags=conf.SEMINARIO_FLICKR_TAG, per_page='10')
+    print "PROCURANDO POR", conf.SEMINARIO_FLICKR_TAG, "NO FLICKR"
+    
+    retorno = []
+    # print "FLICKR \/"
+    # print photos
+    # print photos[0]
+    for photo in photos[0]:
+        obj = {}
+        # print "  ", photo.get('id'), photo.keys()
+        obj['id'] = photo.get('id')
+        obj['title'] = photo.get('title')
+        obj['owner'] = photo.get('owner')
+        # infos = flickr.photos_getInfo(photo_id=photo.get('id'))
+        sizes = flickr.photos_getSizes(photo_id=photo.get('id'))
+        # print "    x", sizes.keys()
+        for size in sizes:
+            # img = size[0]
+            for img in size:
+                print "      ", img.get('source'), img.get('label') #, img.keys()
+                obj[img.get('label')] = img.get('source')
+        retorno.append(obj)
+    # print retorno
+    # print "FLICKR /\\"
+    return retorno
+
 @seminario.route('/')
 def index():
     twitter_tag = conf.SEMINARIO_TWITTER_TAG
     cid = conf.SEMINARIO_CATEGORIA_ID
     pagination, posts = fromcache("seminario_posts") or tocache("seminario_posts", wordpress.getPostsByCategory(cat=cid))
     twites = fromcache("seminario_twitts") or tocache("seminario_twitts", twitts(hashtag=twitter_tag, count=5) )
-    print posts
-    print twites
+    photos = get_flickr_photos()
+    # print posts
+    # print twites
 
-    return render_template('seminario.html', posts=posts, twitts=twites)
+    return render_template('seminario.html', posts=posts, twitts=twites, photos=photos)
