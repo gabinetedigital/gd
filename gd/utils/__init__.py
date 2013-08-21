62a7bcacdc23db9d7767aa789442d833e9c0e833
Sergio Berlotto <sergio-berlotto@gg.rs.gov.br>
2013-08-21 11:03:23 -0300

    (seminario) Adicionada funcao para slug

--- gd/utils/__init__.py
+++ gd/utils/__init__.py
@@ -46,6 +46,23 @@ def _default_handler(value):
         return datetime.isoformat(value)
 
 
+_slugify_strip_re = re.compile(r'[^\w\s-]')
+_slugify_hyphenate_re = re.compile(r'[-\s]+')
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    
+    From Django's "django/template/defaultfilters.py".
+    """
+    import unicodedata
+    if not isinstance(value, unicode):
+        value = unicode(value)
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(_slugify_strip_re.sub('', value).strip().lower())
+    return _slugify_hyphenate_re.sub('-', value)
+
+
 def convert_todatetime(s=None):
 
     # s = 'Tue Mar 29 08:11:25 +0000 2011'
