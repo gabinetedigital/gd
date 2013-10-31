f2d671ce30f5ee257b6b4e4275c6c26c3944da10
Guilherme Guerra <guerrinha@comum.org>
2013-10-31 15:41:21 -0200

    Organizando botões e responsividades da página de vídeos

--- gd/content/static/js/base/video.js
+++ gd/content/static/js/base/video.js
@@ -1,7 +1,9 @@
-/* Copyright (C) 2011  Lincoln de Sousa <lincoln@comum.org>
+/* Copyright (C) 2013  Guilherme Guerra <guerrinha@comum.org>
+ * Copyright (C) 2011  Lincoln de Sousa <lincoln@comum.org>
  * Copyright (C) 2011  Governo do Estado do Rio Grande do Sul
  *
  *   Author: Lincoln de Sousa <lincoln@gg.rs.gov.br>
+ *   Author: Guilherme Guerra <guerrinha@comum.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -42,4 +44,29 @@ $(window).load(function() {
         });
     });
 
+    $('video').mediaelementplayer({});
+
+    $('#facebook').click(function() {
+        window.open(
+            'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),
+            'facebook-share-dialog',
+            'width=626,height=436');
+        return false;
+     });
+
+    $('#twitter').click(function() {
+        window.open(
+            'https://twitter.com/share?url='+encodeURIComponent(location.href),
+            'Twitter-share-dialog',
+            'width=626,height=436');
+        return false;
+     });
+
+    $('#googleplus').click(function() {
+        window.open(
+            'https://plus.google.com/share?url='+encodeURIComponent(location.href),
+            'googleplus-share-dialog',
+            'width=626,height=436');
+        return false;
+     });
 });
