2073722abba068c599c62ac3a83e238d53a14c36
Sergio Berlotto <sergio-berlotto@gg.rs.gov.br>
2014-05-14 15:28:07 -0300

    (novodesign) Combos de filtro e ordenação

--- gd/deolho/static/js/customScripts.js
+++ gd/deolho/static/js/customScripts.js
@@ -1,3 +1,4 @@
+
 $( "#linkSaibaMais" ).click(function() {
 	$( "#additionalInfo" ).toggle( "slide",{direction: 'left'}, 1000 );
 });
@@ -15,5 +16,49 @@ $( "#compartilhaObra" ).click(function() {
 	$( "#compartilhaObraBox" ).toggle( "slide",{direction: 'up'}, 1000 );
 });
 
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
+$(function() {
 
+    //Verifica se está ordenado e ajusta o select
+    var ordenado = GetURLParameter("o");
+    if(ordenado){
+        $('#slordem').val(ordenado);
+    }
 
+    $('#slordem').change(function(ev){
+        var ordem = $(this).val();
+        // window.location.href = window.location.href.replace( /[\?#].*|$/, "?single" );
+        console.log(ev);
+        console.log("ORDEM", ordem);
+        var newurl = "";
+        if(ordem){
+            newurl = window.location.href.replace( /[\?#].*|$/, "?o="+ordem );
+        }else{
+            newurl = window.location.href.replace( /[\?#].*|$/, "" );
+        }
+        console.log("URL", newurl);
+        window.location.href = newurl;
+    });
+
+    $('#slfiltro').change(function(ev){
+        console.log("CHANGED FILTRO!");
+        $('#filtros div').hide();
+        console.log("HIDE FILTRO!");
+        var filtro = $(this).val();
+        console.log("FILTRO:",filtro);
+        $('#div'+filtro).show();
+    });
+
+});
